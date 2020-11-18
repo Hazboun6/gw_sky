a477ed4841bdcd12898e7415b837263fda8ca592
Hazboun6 <jeffrey.hazboun@gmail.com>
2020-11-17 23:02:39 -0800

    check kernel declaration

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -16,6 +16,7 @@ def _exec_notebook(path):
     with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
         args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
                 "--ExecutePreprocessor.timeout=1000",
+                "--ExecutePreprocessor.kernel_name=python3",
                 "--output", fout.name, path]
         subprocess.check_call(args)
 
