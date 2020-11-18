e549f4a9c9d5a395e79e9abbfe019d213748b46b
Hazboun6 <jeffrey.hazboun@gmail.com>
2020-11-17 20:48:43 -0800

    Fixing notebook tests

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -4,8 +4,15 @@
 """Tests for tutorial notebooks."""
 
 import pytest
-import subprocess as sp
+import subprocess
+import tempfile
+
+def _exec_notebook(path):
+    with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
+        args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
+                "--ExecutePreprocessor.timeout=1000",
+                "--output", fout.name, path]
+        subprocess.check_call(args)
 
 def test_pta_smbbh():
-    sp.call('jupyter nbconvert --to notebook --inplace --execute ../docs/_static/notebooks/pta_smbbh.ipynb',
-            shell=True)
+    _exec_notebook('../docs/_static/notebooks/pta_smbbh.ipynb')
